34a03659a3d467b2f37aae992238648955def1d4
co9olguy <co9olguy@users.noreply.github.com>
2020-05-16 14:13:16 -0400

    clean up conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import numpy as np
-import pytest
-
-from pennylane_aqt import AQTSimulatorDevice, AQTNoisySimulatorDevice
 
 np.random.seed(42)
