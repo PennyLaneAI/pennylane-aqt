c6122f74cd1f78fc4aef46aecde98251a34cc409
andrijapau <andrija.paurevic@xanadu.ai>
2024-12-24 11:02:23 -0500

    add pylint disable to ops.py

--- pennylane_aqt/ops.py
+++ pennylane_aqt/ops.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Contains custom AQT operations, designed to be used in PennyLane QNodes."""
+# pylint: disable = too-few-public-methods
 from pennylane.operation import Operation
 
 
