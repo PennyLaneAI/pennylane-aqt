bc07690a6a995a3cf9e5f02ff87e02c9c0909cc4
Yushao Chen (Jerry) <chenys13@outlook.com>
2025-11-03 15:05:37 -0500

    fix tests regarding default config (#102)
    * fix tests regarding default config
    
    * Removed unused setting

--- tests/test_device.py
+++ tests/test_device.py
@@ -458,7 +458,7 @@ class TestAQTDeviceIntegration:
         tmpdir.join("config.toml").write(test_config)
         monkeypatch.setattr("os.curdir", tmpdir)
         monkeypatch.setattr(
-            "pennylane.default_config", qml.Configuration("config.toml")
+            "pennylane.devices.device_constructor.default_config", qml.Configuration("config.toml")
         )  # force loading of config
 
         dev = qml.device("aqt.sim", wires=2)
@@ -482,7 +482,7 @@ class TestAQTDeviceIntegration:
         monkeypatch.setattr("os.curdir", tmpdir.join("folder_without_a_config_file"))
 
         c = qml.Configuration("config.toml")
-        monkeypatch.setattr("pennylane.default_config", c)  # force loading of config
+        monkeypatch.setattr("pennylane.devices.device_constructor.default_config", c)  # force loading of config
 
         dev = qml.device("aqt.sim", wires=2)
 
@@ -500,7 +500,7 @@ class TestAQTDeviceIntegration:
 
         monkeypatch.setattr("os.curdir", tmpdir.join("folder_without_a_config_file"))
         monkeypatch.setattr(
-            "pennylane.default_config", qml.Configuration("config.toml")
+            "pennylane.devices.device_constructor.default_config", qml.Configuration("config.toml")
         )  # force loading of config
 
         dev = qml.device("aqt.sim", wires=2)
